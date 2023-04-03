ad34afcc2b5dab76e8c90c5a465eb7a5a36fd7a9
vlbn <valbuena@duck.com>
2023-04-04 00:06:54 +0200

    bulma OK

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,5 @@ import FancyCursor from "./components/FancyCursor.vue";
 
 <template>
   <FancyCursor />
-  <a href="#">help</a>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, incidunt possimus, ea ipsam eveniet debitis nihil hic itaque sunt tempore quaerat odio quis aliquid ratione laboriosam nostrum dolor ullam illum.
 </template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "locomotive-scroll/src/locomotive-scroll.scss";
+import "./assets/sass/_index.scss";
+
 createApp(App).mount("#app");
