b05d508a859e4a4eede45e87fbe2867559c1f649
vlbn <valbuena@duck.com>
2023-04-05 17:34:13 +0200

    fancy mouse icon OK

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script setup>
 import FancyCursor from "./components/FancyCursor.vue";
+import FancyMouseIcon from "./components/FancyMouseIcon.vue"
 </script>
 
 <template>
-  <FancyCursor />
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, incidunt possimus, ea ipsam eveniet debitis nihil hic itaque sunt tempore quaerat odio quis aliquid ratione laboriosam nostrum dolor ullam illum.
+  <FancyCursor trigger=".pointer" />
+  <FancyMouseIcon position="is-fixed-b-r" />
+
+  <div class="wrapper">
+
+    <!-- slide 1 -->
+    <section class="slide1">
+      <mark>slide <a href="#">1</a></mark>
+    </section>
+
+    <!-- slide 2 -->
+    <section class="slide2">
+      <mark>slide 2</mark>
+    </section>
+
+  </div>
 </template>
