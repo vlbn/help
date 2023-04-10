1a2e4ee6a8ea7bb3c431bab9e63d604f09939445
vlbn <valbuena@duck.com>
2023-04-10 23:35:15 +0200

    gg sass // yay gsap

--- src/components/FancyMouseIcon.vue
+++ src/components/FancyMouseIcon.vue
@@ -1,94 +1,91 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { gsap } from "gsap";
+
+const props = defineProps({
+  direction: {
+    type: String,
+    default() {
+      return "down";
+    },
+  },
+  color: {
+    type: String,
+    default() {
+      return "#42b983";
+    },
+  },
+});
+
+let scrollTl = gsap.timeline({ paused: true });
+
+let theColor = ref(props.color);
+let theDirection = ref(props.direction);
+
+onMounted(() => {
+  gsap.set(".mouse-wheel", {
+    background: theColor.value,
+    autoAlpha: 0,
+  });
+  scrollTl = gsap
+    .timeline({ repeat: -1 })
+    .to(".mouse-wheel", {
+      duration: 0.3,
+      scale: 1.9,
+      autoAlpha: 1,
+    })
+    .to(".mouse-wheel", {
+      duration: 0.5,
+      scale: 1.1,
+      y: 13,
+    })
+    .to(".mouse-wheel", {
+      autoAlpha: 0,
+    })
+    .play();
+});
+</script>
+
 <template>
-  <div class="onTopOfEverything">
-    <div class="mouse-container pointer">
-      <div class="mouse-body">
-        <div class="mouse-wheel"></div>
-      </div>
+  <div class="mouse-container">
+    <div class="mouse-body">
+      <div class="mouse-wheel"></div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped>
-// mouse icon - scroll indicator
-.onTopOfEverything {
+<style scoped>
+.mouse-container {
   z-index: 9999 !important;
+  margin: 3rem;
+  width: 25px;
+  height: 43px;
 }
-
-.pointer {
+.mouse-body {
   cursor: pointer;
-}
-.mouse {
-  &-container {
-    margin: 3rem;
-    width: 25px;
-    height: 43px;
-  }
-
-  &-body {
-    display: block;
-    position: relative;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border: 2px solid #6e6e6e5e;
-    -moz-border-radius: 15px;
-    -webkit-border-radius: 15px;
-    border-radius: 15px;
-  }
-
-  &-wheel {
-    display: block;
-    margin: 0 0 0 -2px;
-    position: relative;
-    top: 10px;
-    left: 50%;
-    width: 4px;
-    height: 4px;
-    background: #6e6e6e5e;
-    -moz-border-radius: 50%;
-    -webkit-border-radius: 50%;
-    border-radius: 50%;
-    -moz-animation: mouse-wheel 1.8s linear 0s infinite normal none;
-    -webkit-animation: mouse-wheel 1.8s linear 0s infinite normal none;
-    animation: mouse-wheel 1.8s linear 0s infinite normal none;
-  }
+  display: block;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0.1em solid #6e6e6e60;
+  -moz-border-radius: 15px;
+  -webkit-border-radius: 15px;
+  border-radius: 15px;
 }
 
-@keyframes mouse-wheel {
-  0% {
-    -moz-transform: translate3d(0, 0, 0);
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-    opacity: 1;
-  }
-
-  35% {
-    -moz-transform: translate3d(0, 7px, 0);
-    -webkit-transform: translate3d(0, 7px, 0);
-    transform: translate3d(0, 7px, 0);
-    opacity: 1;
-  }
-
-  50% {
-    -moz-transform: translate3d(0, 10px, 0);
-    -webkit-transform: translate3d(0, 10px, 0);
-    transform: translate3d(0, 10px, 0);
-    opacity: 0;
-  }
-
-  50.001% {
-    -moz-transform: translate3d(0, 0, 0);
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-    opacity: 0;
-  }
-
-  80% {
-    -moz-transform: translate3d(0, 0, 0);
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
-    opacity: 0;
-  }
+.mouse-wheel {
+  display: block;
+  margin: 0 0 0 -2px;
+  position: relative;
+  top: 5px;
+  left: 50%;
+  width: 4px;
+  height: 4px;
+  background: #6e6e6e;
+  -moz-border-radius: 50%;
+  -webkit-border-radius: 50%;
+  border-radius: 50%;
 }
 </style>
