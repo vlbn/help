b21f3e4b37fc129b7a0ec0546eb0b865efbd3e56
vlbn <valbuena@duck.com>
2024-02-09 18:51:36 +0100

    so much out of date

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import "locomotive-scroll/src/locomotive-scroll.scss";
-import "./assets/sass/_index.scss";
-
-createApp(App).mount("#app");
