66ff1d33a159512d296541ce84a36e63d3c27071
vlbn <valbuena@duck.com>
2024-02-10 15:09:03 +0100

    gg: router, bulma

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 
 import "locomotive-scroll/src/locomotive-scroll.scss";
-import "./assets/scss/_index.scss";
+import "./assets/styles.scss";
 
 const app = createApp(App);
-
-app.use(router);
 
 app.mount("#app");
